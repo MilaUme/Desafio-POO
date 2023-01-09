a210b10cefc0ab8e887e61b5e80dc6aeab2f071a
MilaUme <mila.umehara@gmail.com>
2023-01-09 16:12:56 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import com.br.dio.desafio.dominio.Curso;
-import com.br.dio.desafio.dominio.Mentoria;
+import com.br.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -21,9 +20,43 @@ public class Main {
         mentoria.setData(LocalDate.now());
 
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devMila = new Dev();
+        devMila.setNome("Mila");
+        devMila.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Mila" + devMila.getConteudosInscritos());
+        devMila.progredir();
+        devMila.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Mila" + devMila.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Mila" + devMila.getConteudosConcluidos());
+        System.out.println("XP:" + devMila.calcularTotalXp());
+
+        System.out.println("----");
+
+
+        Dev devEric = new Dev();
+        devEric.setNome("Eric");
+        devEric.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Eric" + devEric.getConteudosInscritos());
+        devEric.progredir();
+        devEric.progredir();
+        devEric.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Eric" + devEric.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Eric" + devEric.getConteudosConcluidos());
+        System.out.println("XP:" + devEric.calcularTotalXp());
+
 
     }
 }
